49039c179d55b2fdfdf3b59fcaa26c1d6391b8f3
Vadim Ramkovich <vadimka.ramkovich@gmail.com>
2026-08-25 21:21:30 +0200

    all task

---
+++ js/task-2.js
@@ -0,0 +1,31 @@
+class Storage {
+    #items = {};
+
+constructor(items){
+    this.#items = items;
+}
+
+getItems(){
+return this.#items
+} 
+addItem (newItem){
+this.#items.push(newItem)
+
+}
+removeItem(ItemToRemove){
+   this.#items = this.#items.filter(item => item !== ItemToRemove )
+
+}
+
+}
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scanner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
